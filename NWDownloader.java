c2f1cf5f38ea770e044552024e6e58f5c8c72e4f
occidere <sj9401h@naver.com>
2016-10-13 13:19:07 +0900

    ver 0.0.2
    * 반복 다운로드 가능하게 수정

--- NWDownloader.java
+++ NWDownloader.java
@@ -19,38 +19,49 @@ public class NWDownloader {
 	private static int pageNum;
 	private static String extension;
 	private static String preNum;
+	private static int pivot = 1;
 	
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("주소를 입력하세요 : "); address = sc.nextLine();
-
-		Document doc = Jsoup.connect(address).userAgent("Mozilla/5.0").get();
-		
-		//parentTitle은 최상위 폴더로 지정할 웹툰 제목 & 특수문자 제거
-		String parentTitle = doc.select("h2.ly_tit").text().replaceAll("[^[:alnum:]+]", " ");
-		//title은 회차수 까지 포함한 최상위 폴더의 내부에 생성될 개별 폴더 & 특수문자 제거
-		String title = doc.select("meta[property=og:title]").attr("content").replaceAll("[^[:alnum:]+]", " ");
-		//path는 최종 다운로드 주소
-		String path = "C:/Webtoon/"+parentTitle+"/"+title+"/";
-		
-		System.out.printf("제목 : %s\n다운로드 폴더 : %s\n",title,path);
-		
-		//파일 다운받을 경로 생성 ex)C:/webtoon/복학왕/복학왕 - 115화/
-		f = new File(path);
-		f.mkdirs();
-		
-		//<img src= 부분 파싱
-		Elements elements = doc.select("img[src~=imgcomic]");
-		//전체 파일 개수
-		int total = elements.size();
-		System.out.printf("다운로드 시작 (전체 %d개)\n", total);
-		String imgUrl = "";
-		for(Element e : elements){
-			imgUrl = e.attr("src");
-			download(path, imgUrl, pageNum);
-			System.out.printf("%d / %d ...... 완료!\n", ++pageNum, total);
+		//0입력시 종료, 1이면 계속 실행
+		while(pivot == 1){
+			Scanner sc = new Scanner(System.in);
+			System.out.print("주소를 입력하세요 : "); address = sc.nextLine();
+			Document doc = Jsoup.connect(address).userAgent("Mozilla/5.0").get();
+			
+			//parentTitle은 최상위 폴더로 지정할 웹툰 제목 & 특수문자 제거
+			String parentTitle = doc.select("h2.ly_tit").text().replaceAll("[^[:alnum:]+]", " ").replaceAll("[.]", "");
+			//title은 회차수 까지 포함한 최상위 폴더의 내부에 생성될 개별 폴더 & 특수문자 제거
+			String title = doc.select("meta[property=og:title]").attr("content").replaceAll("[^[:alnum:]+]", " ").replaceAll("[.]", "");
+			//path는 최종 다운로드 주소
+			String path = "C:/Webtoon/"+parentTitle+"/"+title+"/";
+			
+			System.out.printf("제목 : %s\n다운로드 폴더 : %s\n",title,path);
+			
+			//파일 다운받을 경로 생성 ex)C:/webtoon/복학왕/복학왕 - 115화/
+			f = new File(path);
+			f.mkdirs();
+			
+			//<img src= 부분 파싱
+			Elements elements = doc.select("img[src~=imgcomic]");
+			//전체 파일 개수
+			int total = elements.size();
+			System.out.printf("다운로드 시작 (전체 %d개)\n", total);
+			String imgUrl = "";
+			for(Element e : elements){
+				imgUrl = e.attr("src");
+				download(path, imgUrl, pageNum);
+				System.out.printf("%d / %d ...... 완료!\n", ++pageNum, total);
+			}
+			
+			address = ""; pageNum = 0;
+			System.out.print("done!\n종료는 0, 다른 만화 받기는 1을 입력하세요 : ");
+			pivot = sc.nextByte();
+			
+			if(pivot == 0) sc.close();
+			else if(pivot == 1) continue;
+			else System.out.println("잘못된 입력입니다.");
+			System.out.println("프로그램을 종료합니다.");
 		}
-		System.out.println("done!"); sc.next(); sc.close();
 	}
 	
 	private static void download(String path, String imgUrl, int pageNum) throws Exception {
@@ -68,7 +79,7 @@ public class NWDownloader {
 		
 		FileOutputStream fos = new FileOutputStream(path+preNum+(pageNum+1)+"."+extension);
 		HttpURLConnection conn = (HttpURLConnection)new URL(imgUrl).openConnection();
-		conn.setConnectTimeout(5000); //최대 5초까지 시간 지연 기다려줌
+		conn.setConnectTimeout(10000); //최대 10초까지 시간 지연 기다려줌
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Referer", address); //핵심! 이거 빠지면 연결 안됨
 		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
