efea6f5b9bdf3c45edee5a9ae844fe5b9ce6272b
S. J. Lee <sj9401h@naver.com>
2016-10-13 16:26:46 +0900

    ver 0.1.2

--- NWDownloader.java
+++ NWDownloader.java
@@ -14,21 +14,27 @@ import org.jsoup.select.Elements;
 
 public class NWDownloader {
 	private static final String defaultPath = "C:\\Webtoon\\";
+	private static boolean status = true; //실패 여부 변수
 	public static void main(String[] args) throws Exception {
 		Scanner sc;
 		while(true){
 			int menuSelect, innerSelector;
 			sc = new Scanner(System.in);
-			System.out.println("메뉴를 선택하세요\n1. 한 편씩 다운로드\n2. 여러 편씩 다운로드(beta)\n3. 다운로드 폴더 열기\n9. 종료"); menuSelect = sc.nextInt();
+			System.out.println("메뉴를 선택하세요\n  1. 한 편씩 다운로드\n  2. 여러 편씩 다운로드\n  3. 다운로드 폴더 열기\n  0. 종료"); menuSelect = sc.nextInt();
 			switch(menuSelect){
 				case 1:{
 					innerSelector = 1;
 					while(true){
 						sc = new Scanner(System.in);
-						System.out.print("주소를 입력하세요 : "); connector(sc.nextLine());
+						System.out.print("주소를 입력하세요 : ");{
+							connector(sc.nextLine());
+							//실패시 메뉴로 돌아가기
+							if(!status){
+								System.out.println("다운로드 실패! 메뉴로 돌아갑니다.\n"); break;
+							}
+						}
 						System.out.print("종료는 0, 다른 만화 다운로드는 1을 입력하세요 : "); innerSelector = sc.nextInt();
 						if(innerSelector == 0) break;
-						System.out.println();
 					}
 					break;
 				}
@@ -54,11 +60,15 @@ public class NWDownloader {
 						}
 						
 						System.out.printf("총 회차수 %d개\n", (e-s+1));
-						for(int i=s;i<=e;i++) connector(address+i);
+						
+						//실패시 자동 다음화 다운로드 시도
+						for(int i=s;i<=e;i++){
+							connector(address+i);
+							if(!status) System.out.println("다운로드 실패! 다음화 다운로드를 시도합니다...");
+						}
 						
 						System.out.print("종료는 0, 다른 만화 다운로드는 1을 입력하세요 : "); innerSelector = sc.nextInt();
 						if(innerSelector == 0) break;
-						System.out.println();
 					}
 					break;
 				}
@@ -67,45 +77,53 @@ public class NWDownloader {
 					Runtime.getRuntime().exec("explorer.exe "+defaultPath);
 					break;
 				}
-				case 9: System.out.println("프로그램을 종료합니다.");
+				case 0: System.out.println("프로그램을 종료합니다.");
 				default: sc.close(); return;
 			}
 		}
 	}
-	private static void connector(String address) throws Exception {
-		
-		//타임아웃 10초
-		Document doc = Jsoup.connect(address).userAgent("Mozilla/5.0").timeout(10000).get();
-		
-		//parentTitle은 최상위 폴더로 지정할 웹툰 제목 & 특수문자 제거
-		String parentTitle = doc.select("h2.ly_tit").text().replaceAll("[^[:alnum:]+]|[.]|[?]", " ").trim();
-		//title은 회차수 까지 포함한 최상위 폴더의 내부에 생성될 개별 폴더 & 특수문자 제거
-		String title = doc.select("meta[property=og:title]").attr("content").replaceAll("[^[:alnum:]+]|[.]|[?]", " ").trim();
-		//path는 최종 다운로드 주소
-		String path = defaultPath+parentTitle+"/"+title+"/";
-		int pageNum = 0;
-		
-		System.out.printf("제목 : %s\n다운로드 폴더 : %s\n",title,path);
-		
-		//파일 다운받을 경로 생성 ex)C:/webtoon/복학왕/복학왕 - 115화/
-		File f = new File(path);
-		f.mkdirs();
-		
-		//<img src= 부분 파싱
-		Elements elements = doc.select("img[src~=imgcomic]");
-		//전체 파일 개수
-		int total = elements.size();
-		System.out.printf("다운로드 시작 (전체 %d개)\n", total);
-		String imgUrl = "";
-		for(Element e : elements){
-			imgUrl = e.attr("src");
-			download(address, path, imgUrl, pageNum);
-			System.out.printf("%2d / %2d ...... 완료!\n", ++pageNum, total);
+	private static void connector(String address) {
+		try{
+			//타임아웃 30초
+			Document doc = Jsoup.connect(address).userAgent("Mozilla/5.0").timeout(30000).get();
+			
+			//parentTitle은 최상위 폴더로 지정할 웹툰 제목 & 특수문자 제거 & 정규식 수정
+			String parentTitle = doc.select("h2.ly_tit").text().replaceAll("[\\/:*?<>|.]", " ").trim();
+			//title은 회차수 까지 포함한 최상위 폴더의 내부에 생성될 개별 폴더 & 특수문자 제거 & 정규식 수정
+			String title = doc.select("meta[property=og:title]").attr("content").replaceAll("[\\/:*?<>|.]", " ").trim();
+			//path는 최종 다운로드 주소
+			String path = defaultPath+parentTitle+"\\"+title+"\\";
+			int pageNum = 0;
+			
+			System.out.printf("제목 : %s\n다운로드 폴더 : %s\n",title,path);
+			
+			//파일 다운받을 경로 생성 ex)C:/webtoon/복학왕/복학왕 - 115화/
+			File f = new File(path);
+			f.mkdirs();
+			
+			//<img src= 부분 파싱
+			Elements elements = doc.select("img[src~=imgcomic]");
+			//전체 파일 개수
+			int total = elements.size();
+			System.out.printf("다운로드 시작 (전체 %d개)\n", total);
+			String imgUrl = "";
+			for(Element e : elements){
+				imgUrl = e.attr("src");
+				download(address, path, imgUrl, pageNum);
+				
+				if(!status) return;
+				
+				System.out.printf("%2d / %2d ...... 완료!\n", ++pageNum, total);
+			}
+			status = true;
 		}
-		address = ""; pageNum = 0;
+		catch(Exception e){
+			status = false;
+			return;
+		}
 	}
 	
-	private static void download(String address, String path, String imgUrl, int pageNum) throws Exception {
+	private static void download(String address, String path, String imgUrl, int pageNum) {
 		String extension = "", preNum = "";
 		
 		//확장자 판단
@@ -119,18 +137,27 @@ public class NWDownloader {
 		if(pageNum+1<10) preNum = "00";
 		else if(pageNum+1<100) preNum = "0";
 		
-		FileOutputStream fos = new FileOutputStream(path+preNum+(pageNum+1)+"."+extension);
-		HttpURLConnection conn = (HttpURLConnection)new URL(imgUrl).openConnection();
-		conn.setConnectTimeout(10000); //최대 10초까지 시간 지연 기다려줌
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Referer", address); //핵심! 이거 빠지면 연결 안됨
-		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-		InputStream in = conn.getInputStream();
-		
-		//다운로드 부분. 버퍼 크기 16*1024B(16Kb)로 조정
-		byte[] buf = new byte[16*1024];
-		int len = 0;
-		while((len = in.read(buf))>0) fos.write(buf, 0, len);
-		fos.close();
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(path+preNum+(pageNum+1)+"."+extension);
+			HttpURLConnection conn = (HttpURLConnection)new URL(imgUrl).openConnection();
+			conn.setConnectTimeout(30000); //최대 30초까지 시간 지연 기다려줌
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Referer", address); //핵심! 이거 빠지면 연결 안됨
+			conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+			InputStream in = conn.getInputStream();
+			
+			status = true;
+			
+			//다운로드 부분. 버퍼 크기 32*1024B(32Kb)로 조정
+			byte[] buf = new byte[32768];
+			int len = 0;
+			while((len = in.read(buf))>0) fos.write(buf, 0, len);
+			fos.close();
+		} 
+		catch (Exception e) {
+			status = false;
+			return;
+		}
 	}
 }
